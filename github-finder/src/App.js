73c1f673c369b10a5c36776570f776116bb10cb3
Maggie Kuo <maggie.kuo761@gmail.com>
2020-02-17 16:18:49 -0800

    learning-jsx

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -3,23 +3,35 @@ import './App.css';
 
 // using a class-based React component. To render or return anything, the function has to be called render()
 
-class App extends Component {
-// all renders must be wrapped in one parent element, such as div
-// But what if you don't want your render to be inside a div? You can wrap it inside a React Fragment element
-  render(){
-    return (
-      <Fragment>
-        <h1>I'm a fragment</h1>
-        <h2>I'm a fragment too</h2>
-        <h3>We have no parent div</h3>
-        <p>React.Fragment allows us to do that</p>
-        <p>I love React!</p>
-      </Fragment>
 
-      
+class App extends Component {
+  // you can add variables from methods within this class
+  message = () => 'Take me to your leader!';
+
+  render() {
+    //or you can add variables for render within the render method
+    const name = 'Jane Doe';
+    const showName = true;
+    const loading = false;
+    const foo = () => 'Bar Bazz';
+    
+    // the { } can support any javascript expression within it, even rendering whole functions.
+    // Most common expression: the ternary operator-- variable ? true : false
+    return (
+      <div className="app">
+        <h2>{foo()}</h2>
+        {/* The && is an "and" conditional. It will only show name if showName is true and
+        previous condition, loading is false, is satisfied. */}
+
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>} 
+
+        {/* Because of scoping, message() doesn't work. Message sits outside of render().
+        You have to invoke this */}
+        <h3>{this.message()}</h3>
+      </div>
     );
   }
-
 }
 
-export default App;
+export default App
+
